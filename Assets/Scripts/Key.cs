16f23d9d9ece0d67df5dd58b16c788d469406fa6
Silpux <101855190+Silpux@users.noreply.github.com>
2024-11-27 05:19:20 +0200

    Identify player hit by component

--- Assets/Scripts/Key.cs
+++ Assets/Scripts/Key.cs
@@ -7,7 +7,7 @@ public class Key : MonoBehaviour{
 
     private void OnTriggerEnter2D(Collider2D other){
 
-        if(other.gameObject.CompareTag("Player")){
+        if(other.gameObject.TryGetComponent<Player>(out Player _)){
 
             Destroy(this.gameObject);
             Destroy(ClosedObject.gameObject);
