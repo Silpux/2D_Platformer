a42bee0e0e6ef649fcb80fc974b7137f1c06a3d8
Silpux <101855190+Silpux@users.noreply.github.com>
2024-11-29 22:26:02 +0200

    move prefabs to folders, add ui sprites, add levels panel
    move prefabs to folders, add ui sprites

---
+++ Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -0,0 +1,19 @@
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[CreateAssetMenu(fileName = "LevelData", menuName = "Game/Level Data")]
+public class LevelData : ScriptableObject{
+
+    public List<LevelInfo> levels;
+
+}
+
+[Serializable]
+public class LevelInfo{
+
+    [SerializeField] private string name;
+    [SerializeField] private SceneAsset scene;
+
+}
