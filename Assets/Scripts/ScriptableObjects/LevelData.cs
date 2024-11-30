74b86b20e4f358688afa71e2a757dfff51898642
Silpux <101855190+Silpux@users.noreply.github.com>
2024-11-30 02:38:56 +0200

    Update to work on build

--- Assets/Scripts/ScriptableObjects/LevelData.cs
+++ Assets/Scripts/ScriptableObjects/LevelData.cs
@@ -16,8 +16,12 @@ public class LevelInfo{
     [SerializeField] private string name;
     public string Name => name;
 
+    #if UNITY_EDITOR
+
     [SerializeField] private SceneAsset scene;
     public SceneAsset Scene => scene;
+
+    #endif
 
     [HideInInspector]
     public string scenePath;
