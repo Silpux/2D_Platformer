b9d66897115af84ff282e063c3c2027970dec580
Silpux <101855190+Silpux@users.noreply.github.com>
2024-12-06 04:35:08 +0200

    add settings to set music volume

--- Assets/Scripts/UI/SettingsPanel.cs
+++ Assets/Scripts/UI/SettingsPanel.cs
@@ -1,5 +1,29 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsPanel : Panel{
+
+    [SerializeField] private AudioMixer audioMixer;
+
+    private const string MusicGroupParam = "MusicGroup";
+    private const string MusicParam = "MusicVolume";
+    private const string SFXGroupParam = "SFXGroup";
+    private const string SFXParam = "SFXVolume";
+
+    public void SetMusicValue(float value){
+        audioMixer.SetFloat(MusicParam, Mathf.Log10(value) * 20);
+    }
+
+    public void ToggleMusic(bool state){
+        audioMixer.SetFloat(MusicGroupParam, state ? 0f : -80f);
+    }
+
+    public void SetSFXValue(float value){
+        audioMixer.SetFloat(SFXParam, Mathf.Log10(value) * 20);
+    }
+
+    public void ToggleSFX(bool state){
+        audioMixer.SetFloat(SFXGroupParam, state ? 0f : -80f);
+    }
 
 }
