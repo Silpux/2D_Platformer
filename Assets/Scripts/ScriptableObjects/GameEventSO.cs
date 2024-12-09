5b65e209db0741e2ced61628ba806acb61556c1b
Silpux <101855190+Silpux@users.noreply.github.com>
2024-12-09 17:13:23 +0200

    hide listeners list in event

--- Assets/Scripts/ScriptableObjects/GameEventSO.cs
+++ Assets/Scripts/ScriptableObjects/GameEventSO.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "GameEventSO", menuName = "Scriptable Objects/GameEvent")]
 public class GameEventSO : ScriptableObject{
 
-    public List<GameEventListener> listeners = new List<GameEventListener>();
+    private List<GameEventListener> listeners = new List<GameEventListener>();
 
     public void RaiseEvent(){
         for(int i = listeners.Count - 1;i>=0;i--){
